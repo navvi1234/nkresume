554f863bb212ffa8112606958b1436309f66418b
Naveen <63560717+navvi1234@users.noreply.github.com>
2020-09-11 12:37:42 +0530

    Update settings.py

--- signin/settings.py
+++ signin/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 t_d=os.path.join(BASE_DIR,'templates')
-s_d=os.path.join(BASE_DIR,'static')
+Static_dir=os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [Static_dir]
 
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 #LOGIN_REDIRECT_URL='dashboard'
